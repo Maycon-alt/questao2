92b912656a21a0cec6e4dc49b629b1e8fd822599
Maycon-alt <54949734+Maycon-alt@users.noreply.github.com>
2020-10-01 20:58:04 -0300

    Update arvoredebuscabin.cpp

--- arvoredebuscabin.cpp
+++ arvoredebuscabin.cpp
@@ -1,65 +1,11 @@
-#include <iostream>
-
-
-
-typedef struct node{
-    int chave;
-    struct node* pai;
-    struct node* esq;
-    struct node* dir;
-}node;
-
-int main()
-{
-    bool certo;
-    node* raiz=NULL; int aux;
-    printf("Bem vindo! Comece criando um no, no caso a raiz\n");
-    scanf("%d", &aux);
-    raiz = new node;
-    raiz->chave = aux; raiz->pai=NULL; raiz->esq=NULL; raiz->dir=NULL;
-    
-    do{
-        printf("Digite:\n1-para inserir o no\n2-deletar um no\n3-para imprimir\n0-para sair\n");
-        scanf("%d", &aux);
-        
-        if(aux==1){
-            node* n;
-            printf("Digite o valor do no a ser criado:\n");
-            scanf("%d", &aux);
-            n = new node;
-            n->chave = aux; n->pai=NULL; n->esq=NULL; n->dir=NULL;
-            tree_insert(r, n);
-        }
-        
-    }while(aux!=0);
-
-    return 0;
+package tabela;
+public interface AppTabela {
+    public abstract void consultarTime();
+    public abstract void jogo();
+    public abstract void consultarTabela();
+    public abstract void verJogador(); 
+    public abstract void verTecnico();
 }
-
-
-//funcoes
-node* tree_insert(node* raiz, node* n){
-    node* y=NULL; node* x=raiz;
-    while(x!=NULL){
-        y=x;
-        if(n->chave < x->chave) x = x->esq;
-        else x = x->dir;
-    }
-    n->pai = y;
-    if(y==NULL)raiz = n;
-    else if(n->chave < y->chave) y->esq = n;
-    else y->dir = n;
-}
-
-void transplant(node* raiz, node* u, node* v){
-    if(u->pai==NULL) raiz = v;
-    else if(u==u->pai->esq) u->pai->esq = v;
-    else u->pai->dir = v;
-    if(v!=NULL){
-        v->pai = u->pai;
-    }
-};
-
 
 
 void tree_delete(node* z, node* raiz){
