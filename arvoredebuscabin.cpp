cdb76b0c0238fa10818817ba8d878d41259d0ebe
Maycon-alt <54949734+Maycon-alt@users.noreply.github.com>
2020-10-01 20:58:23 -0300

    Update arvoredebuscabin.cpp

--- arvoredebuscabin.cpp
+++ arvoredebuscabin.cpp
@@ -6,22 +6,3 @@ public interface AppTabela {
     public abstract void verJogador(); 
     public abstract void verTecnico();
 }
-
-
-void tree_delete(node* z, node* raiz){
-    if(z->esq==NULL) transplant(raiz, z, z->dir);
-    else if(z->dir==NULL) transplant(raiz, z, z->esq);
-    else{
-        node* y = tree_minimum(z->dir);
-        if(y->pai!=z){
-            transplant(raiz, y, y->dir);
-            y->dir = z->dir;
-            y->dir->pai = y;
-        }
-        transplant(raiz, z, y);
-        y->esq = z->esq;
-        y->esq->pai = y;
-    }
-}
-
-
